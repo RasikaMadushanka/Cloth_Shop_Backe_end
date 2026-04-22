30da22f79e42830ac47467150eda97d811507bd9
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-04-23 00:50:57 +0530

    add sale entity

--- src/main/java/edu/icet/ecom/model/entity/AdminEntity.java
+++ src/main/java/edu/icet/ecom/model/entity/AdminEntity.java
@@ -1,7 +1,6 @@
 package edu.icet.ecom.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,5 +10,18 @@ import lombok.Setter;
 @Table(name = "admin")
 
 public class AdminEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer adminId;
+
+    @Column(unique = true)
+    private String username;
+    private String password; // Should be hashed
+    private String role;
+    private String fullName;
+    private String nic;
+    private String address;
+    private String lastLoginTime;
+    private Boolean isActive;
+
 }
