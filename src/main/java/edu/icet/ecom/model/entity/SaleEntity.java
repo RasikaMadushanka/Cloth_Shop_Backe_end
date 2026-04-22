4ef54db28c8e278dfb02cce115e5c654e507baa1
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-04-23 00:53:51 +0530

    add saleItems entity

--- src/main/java/edu/icet/ecom/model/entity/SaleEntity.java
+++ src/main/java/edu/icet/ecom/model/entity/SaleEntity.java
@@ -1,4 +1,28 @@
 package edu.icet.ecom.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table (name = "sales")
 public class SaleEntity {
+    @Id
+    private String saleId;
+
+    private Double totalAmount;
+    private Double netAmount;
+    private String paymentMethod;
+    private String timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id")
+    private AdminEntity admin;
+
+    @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL)
+    private List<SalesItemEntity> items;
 }
