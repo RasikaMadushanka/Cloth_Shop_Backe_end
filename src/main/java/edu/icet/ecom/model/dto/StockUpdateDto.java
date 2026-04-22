1ef79290c33cc77da9f39479fde4d2943044424b
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-04-22 23:23:03 +0530

    add productEntity

--- src/main/java/edu/icet/ecom/model/dto/StockUpdateDto.java
+++ src/main/java/edu/icet/ecom/model/dto/StockUpdateDto.java
@@ -1,4 +1,14 @@
 package edu.icet.ecom.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class StockUpdateDto {
+    private String varientId;
+    private String barcodeId;
+    private Integer quantityAdded;
+    private String updateReason; // e.g., "Restock", "Correction", "Return"
+    private String date;
 }
