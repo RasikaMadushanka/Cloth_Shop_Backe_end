d13ec6a407c57bd5b01862aa1ab09f4f236c351a
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-04-23 19:10:12 +0530

    make stock service

--- src/main/java/edu/icet/ecom/controller/Stock_Controller.java
+++ src/main/java/edu/icet/ecom/controller/Stock_Controller.java
@@ -1,9 +1,12 @@
 package edu.icet.ecom.controller;
 
+import edu.icet.ecom.model.dto.StockReportDto;
+import edu.icet.ecom.model.dto.StockUpdateDto;
+import edu.icet.ecom.service.StockService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -11,5 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @CrossOrigin
 public class Stock_Controller {
-    
+    private final StockService stockService;
+
+    @PostMapping("/update")
+    public ResponseEntity<String>updateStock(@RequestBody StockUpdateDto stockUpdateDto){
+        stockService.updateStock(stockUpdateDto);
+        return ResponseEntity.ok("Stock updated successfully");
+    }
+    @GetMapping("reports/daily")
+    public ResponseEntity<StockReportDto>getDaily(@RequestParam String date){
+        return ResponseEntity.ok(stockService.generateReport("Daily",date));
+    }
+    @GetMapping("reports/monthly")
+    public ResponseEntity<StockReportDto>getMonthly(@RequestParam String date){
+        return ResponseEntity.ok(stockService.generateReport("Monthly",date));
+    }
+    @GetMapping("reports/Yearly")
+    public ResponseEntity<StockReportDto>getYearly(@RequestParam String date){
+        return ResponseEntity.ok(stockService.generateReport("Yearly",date));
+    }
+
+
 }
---
+++ src/main/java/edu/icet/ecom/service/StockService.java
@@ -0,0 +1,13 @@
+package edu.icet.ecom.service;
+
+import edu.icet.ecom.model.dto.StockReportDto;
+import edu.icet.ecom.model.dto.StockUpdateDto;
+import org.jspecify.annotations.Nullable;
+
+public class StockService {
+    public void updateStock(StockUpdateDto stockUpdateDto) {
+    }
+
+    public  StockReportDto generateReport(String daily, String date) {
+    }
+}
