9fab35b295ed5853dddeb84b608d5bfe7d48518b
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-04-23 18:50:29 +0530

    make stock controller

---
+++ src/main/java/edu/icet/ecom/controller/Stock_Controller.java
@@ -0,0 +1,15 @@
+package edu.icet.ecom.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/stock")
+@RequiredArgsConstructor
+@CrossOrigin
+public class Stock_Controller {
+    
+}
