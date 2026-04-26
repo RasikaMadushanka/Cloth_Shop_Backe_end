51931015b66df6eb2cdbe996d44d572d8218fb5f
RasikaMadushanka <rasikamadushanka2005@gmail.com>
2026-04-26 20:55:41 +0530

    make admin service

--- src/main/java/edu/icet/ecom/controller/Admin_Controller.java
+++ src/main/java/edu/icet/ecom/controller/Admin_Controller.java
@@ -1,4 +1,38 @@
 package edu.icet.ecom.controller;
 
+import edu.icet.ecom.model.dto.AdminDto;
+import edu.icet.ecom.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin()
+
 public class Admin_Controller {
+    private final AdminService adminService;
+
+    @PostMapping("/add")
+    public ResponseEntity<String> AddAdmin(@RequestBody AdminDto adminDto){
+        adminService.AddAdmin(adminDto);
+        return ResponseEntity.ok("Admin added successfully");
+
+    }
+    @PutMapping("/update/{id}")
+        public ResponseEntity<String> UpdateAdmin(@PathVariable Integer id ,@RequestBody AdminDto adminDto){
+            adminDto.setAdminId(id);
+            adminService.UpdateAdmin(adminDto);
+            return ResponseEntity.ok("Admin updated successfully");
+        }
+
+    @DeleteMapping("/delete/{id}")
+        public ResponseEntity<String> DeleteAdmin(@PathVariable Integer id) {
+        adminService.DeleteAdmin(id);
+        return ResponseEntity.ok("Admin deleted successfully");
+    }
+
 }
---
+++ src/main/java/edu/icet/ecom/service/AdminService.java
@@ -0,0 +1,19 @@
+package edu.icet.ecom.service;
+
+import edu.icet.ecom.model.dto.AdminDto;
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import java.io.Serializable;
+
+public class AdminService {
+
+    public void DeleteAdmin(Integer id) {
+    }
+
+    public void UpdateAdmin(AdminDto adminDto) {
+    }
+
+    public void AddAdmin(AdminDto adminDto) {
+    }
+}
